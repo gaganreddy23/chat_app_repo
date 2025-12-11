3d7d37fdf42276778784cdfb877d62ebc02511c5
gaganreddy23 <mgaganreddy1@gmail.com>
2025-12-12 03:56:24 +0530

    so many due to deployment

--- SPE_Majorproject-main/client/src/pages/RegisterPage.js
+++ SPE_Majorproject-main/client/src/pages/RegisterPage.js
@@ -166,7 +166,7 @@ const RegisterPage = () => {
 
           </form>
 
-          <p className='my-3 text-center'>Already have account is yes please Login ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+          <p className='my-3 text-center'>Already have account if i have one do it  ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
         </div>
     </div>
   )
