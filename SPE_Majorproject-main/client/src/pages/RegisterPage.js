998b804fe4aa9d0db967ffc8d2a7862f0ddbe937
gaganreddy23 <mgaganreddy1@gmail.com>
2025-12-12 04:42:58 +0530

    ome more time

--- SPE_Majorproject-main/client/src/pages/RegisterPage.js
+++ SPE_Majorproject-main/client/src/pages/RegisterPage.js
@@ -166,7 +166,7 @@ const RegisterPage = () => {
 
           </form>
 
-          <p className='my-3 text-center'> do the login one fuck you  ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+          <p className='my-3 text-center'> do the login one more time  ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
         </div>
     </div>
   )
