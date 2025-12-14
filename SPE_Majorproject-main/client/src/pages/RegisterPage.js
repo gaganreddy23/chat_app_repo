cdf7370333559e2252f0a8833437c316b69d4314
gaganreddy23 <mgaganreddy1@gmail.com>
2025-12-14 18:34:12 +0530

    ...

--- SPE_Majorproject-main/client/src/pages/RegisterPage.js
+++ SPE_Majorproject-main/client/src/pages/RegisterPage.js
@@ -166,7 +166,7 @@ const RegisterPage = () => {
 
           </form>
 
-          <p className='my-3 text-center'> doing and jhg i oijhagain again? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+          <p className='my-3 text-center'> doing again ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
         </div>
     </div>
   )
